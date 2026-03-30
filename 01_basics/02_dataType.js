4c67b0f4ae56c41c8d894ec20c8fc5af02fb164b
free916 <manjaykumars259@gmail.com>
2026-03-30 12:38:04 +0000

    datatype02

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer varsion
+
+// alert(3 + 3)//we are using nodejs, not browser
+
+console.log(3
+    +3
+) //code reabability should  be high
+console.log('hitesh');
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+// bigint
+// String = ""
+// Boolean => true/false
+// null =>standalone value
+// undefined 
