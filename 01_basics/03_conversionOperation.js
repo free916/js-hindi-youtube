e2db7de6771c7204acafbc72c257e5920f743910
free916 <manjaykumars259@gmail.com>
2026-03-31 05:35:40 +0000

    conversionOperation datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+// let score = 33
+let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33ad" => nan
+// true = 1; flase=>0
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+//  1=> true; 0=> flase
+// ""=> flase
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
