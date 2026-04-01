95827751cc741c303b75856836380324108d3137
free916 <manjaykumars259@gmail.com>
2026-04-01 11:26:33 +0000

    stack (primitive) haep (non-primitive)

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -32,4 +32,27 @@ const myfunction = function(){
     console.log("hello word");
 
 }
-console.log(typeof scoreValue);
+// console.log(typeof scoreValue);
+    // *****************************************************    //
+    // stack (primitive), haep (non-primitive)
+    let myyoutubname = "hitesh123"
+
+    let anothername = myyoutubname
+    anothername = "chaicode"
+    // console.log(myyoutubname);
+    // console.log(anothername);
+
+    let userOne = {
+        email:"hitesh@gmail.com",
+         upiId:"hitesh@123"
+    }
+    let userTwo = userOne
+    let userThree = userOne
+    
+    userTwo.email = "hitesh12@gmail.com"
+    userThree.upiId = "hitesh12@g"
+
+    console.log(userOne.email);
+    console.log(userTwo.email);
+    console.log(userThree.upiId);
+    
